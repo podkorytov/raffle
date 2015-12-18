b76ef8d4062f0998076548f13cd7b96a52453706
Anton Zakharov <a.zakharov@e1.vsemayki.ru>
2015-12-18 16:31:10 +0600

    нян кэт

--- public/media/js/instafeed.js
+++ public/media/js/instafeed.js
@@ -5,6 +5,7 @@ var hashTag = 'vsemayki';
 var feedLength = 3;
 var $feed = $('#instafeed');
 var token = '19182223.6e63162.3f6b8b2a656e446f99519374978261b0';
+var selectTimer = 13;
 
 function getFeed() {
     $feed.find('div').remove();
@@ -30,7 +31,52 @@ function getFeed() {
         }
     })
 }
+var selectInterval;
+var winner;
+function startRaffle(num) {
+    audio(true);
+    $('#users .user').animate({'width' : '6.5%'}, 200);
+    setTimeout(function() {
+        $feed.animate({'width' : 0}, 200);
+        $('#users').animate({'width' : '100%'}, 200);
+    }, 200);
+    winner = num;
+    setTimeout(function() {
+        selectInterval = setInterval(userSelect, 100);
+    },4050)
+}
+var t=0;
+function userSelect() {
+    if (t<(selectTimer * 10)) {
+        var rand = Math.floor(Math.random() * 90);
+        var $elem = $('.user').eq(rand);
+        $($elem).addClass('hover');
+        setTimeout(function() {
+            $($elem).removeClass('hover');
+        },100);
+        t++;
+    } else {
+        clearInterval(selectInterval);
+        var $elem = $('.user').eq(winner);
+        $($elem).addClass('hover-blue');
+        showWinner($elem.find('img').attr('src'));
+        audio(false);
+    }
+}
+
 
 $(function() {
     getFeed();
+    for (var i=0; i<90; i++) {
+        $('<div class="user"><img src="https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/s750x750/sh0.08/e35/12394029_1623922857868593_205865308_n.jpg"></div>').appendTo('#users');
+    }
 });
+
+function audio(start) {
+    var audio = document.getElementsByTagName("audio")[0];
+    if (start) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
